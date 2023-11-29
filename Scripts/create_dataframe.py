908f76e0d0430815e1a3b750a04abcadb77d75b0
alpaz1 <alpaz@ucm.es>
2023-11-29 16:19:27 +0100

    Pad y gray_scale en imágenes. Inicio clasificador.

--- Scripts/create_dataframe.py
+++ Scripts/create_dataframe.py
@@ -6,34 +6,57 @@ import numpy as np
 import joblib
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib.offsetbox import TextArea, OffsetImage, AnnotationBbox
+from PIL import Image
+from sklearn import datasets, metrics, svm
+from sklearn.model_selection import train_test_split
+
+def plot_image_with_label(row):
+    # Read image
+    image_path = row['filename']
+    image = Image.open(image_path)
+
+    # Plot image
+    fig, ax = plt.subplots()
+    ax.imshow(image)
+    ax.axis('off')
+
+    # Add label below the image
+    label = row['label']
+    label_box = TextArea(f'Label: {label}', textprops=dict(color='black', size=10))
+    ab = AnnotationBbox(label_box, (0.5, -0.05), frameon=False, boxcoords="axes fraction")
+    ax.add_artist(ab)
+
+    # Show the plot
+    plt.show()
+
 
 def create_dataframe(data_folder, width=200, height=None):
     height = height if height is not None else width
     data = dict()
-    data['description'] = 'resized ({0}x{1}) fracture images in rgb'.format(int(width), int(height))
+    data['description'] = 'resized ({0}x{1}) grayscale fracture images'.format(int(width), int(height))
     data['label'] = []
-    #data['filename'] = []
+    data['filename'] = []
     data['data'] = []
 
     #pklname = f"{pklname}_{width}x{height}px.pkl"
 
-    images_folder = os.path.join(data_folder, 'images')
+    images_folder = os.path.join(data_folder, 'padded_images')
     labels_folder = os.path.join(data_folder, 'labels_fractura')
-    print(labels_folder)
 
-    for file in os.listdir(images_folder):
-        im = imread(os.path.join(images_folder, file))
-        im = resize(im, (width, height))  # [:,:,::-1]
-        label_path = os.path.join(labels_folder, os.path.splitext(file)[0] + '.txt')
+    for img in os.listdir(images_folder):
+        im = imread(os.path.join(images_folder, img), as_gray=True)
+        #im = resize(im, (width, height))  # [:,:,::-1]
+        label_path = os.path.join(labels_folder, os.path.splitext(img)[0] + '.txt')
         label = 0
         if os.path.exists(label_path):
             with open(label_path, 'r') as file:
                 label = int((file.readlines()[0]).split()[0])
         else:
-            print(f"Image: {file}, label file not found.")
+            print(f"Image: {img}, label file not found.")
 
         data['label'].append(label)
-        #data['filename'].append(file)
+        data['filename'].append(os.path.join(images_folder, img))
         data['data'].append(im)
 
     df = pd.DataFrame.from_dict(data)
@@ -44,15 +67,72 @@ def create_dataframe(data_folder, width=200, height=None):
     #with open(os.path.join(destination_folder, f'{pklname}.pickle'), 'wb') as file:
     #    pickle.dump(data, file, protocol=pickle.HIGHEST_PROTOCOL)
 
+def flatten_array(array):
+    return array.flatten()
+
 def main():
     data_folder = "./Datasets/Dataset/Femurs"
 
 
-    df = create_dataframe(data_folder)
+    df = create_dataframe(data_folder, 183, 299)
+    print(df['data'][0].shape)
+    # Apply the function to the DataFrame column
+    df['data'] = df['data'].apply(flatten_array)
     pklname = 'df.pkl'
-    df.to_pickle(pklname)
+    #df.to_pickle(pklname)
 
     print(df.head())
+    fig, axes = plt.subplots(1, 5, figsize=(15, 3))
+    
+
+    # Iterate through the first 5 rows of the DataFrame
+    for i, (_, row) in enumerate(df.head(5).iterrows()):
+        # Read image
+        image_path = row['filename']
+        image = Image.open(image_path)
+
+        # Plot image
+        axes[i].imshow(image)
+        axes[i].axis('off')
+
+        # Add label below the image
+        label = row['label']
+        axes[i].text(0.5, -0.05, f'Label: {label}', color='black', size=10, ha='center', va='center', transform=axes[i].transAxes)
+
+    # Adjust layout and show the plot
+    plt.tight_layout()
+    #plt.show()
+
+    clf = svm.SVC(gamma=0.001)
+
+    digits = datasets.load_digits()
+    print(digits.images[0])
+
+    n_samples = len(df.label)
+    flattened_data = np.array([item.flatten() for sublist in df.data.values for item in sublist])
+
+
+    print(flattened_data.shape)
+    print(df.label.values.shape)
+    # Split data into 50% train and 50% test subsets
+    X_train, X_test, y_train, y_test = train_test_split(
+        flattened_data, df.label.values, test_size=0.2, shuffle=False
+    )
+
+    # Learn the digits on the train subset
+    clf.fit(X_train, y_train)
+
+    # Predict the value of the digit on the test subset
+    predicted = clf.predict(X_test)
+    print(
+        f"Classification report for classifier {clf}:\n"
+        f"{metrics.classification_report(y_test, predicted)}\n"
+    )
+    disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
+    disp.figure_.suptitle("Confusion Matrix")
+    print(f"Confusion matrix:\n{disp.confusion_matrix}")
+
+    plt.show()
 
     """"
     data = joblib.load(f'{pklname}.pkl')
