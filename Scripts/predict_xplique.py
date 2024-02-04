d018a88ca6b9e76d350ea6fce7778f4afe85b6ac
equeipodlb <equeipodlb@gmail.com>
2024-02-04 18:24:20 +0100

    Prep fracturas AO
    Recorte de tornillos

--- Scripts/predict_xplique.py
+++ Scripts/predict_xplique.py
@@ -91,7 +91,7 @@ def predict_xplique(load_path, width, height):
 
     preprocess = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        #transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
     X_preprocessed = torch.stack([preprocess(x) for x in X])
@@ -127,4 +127,4 @@ def predict_xplique(load_path, width, height):
         print("\n")
     
 
-predict_xplique(load_path='../models/resnet100.pt', width=224, height=224)
+predict_xplique(load_path='../models/lastresnet', width=224, height=224)
--- Scripts/tifjpg.py
+++ Scripts/tifjpg.py
@@ -6,9 +6,8 @@ from PIL import Image
 def convert_tif_to_jpg_recursive(input_folder):
     for root, dirs, files in os.walk(input_folder):
         for filename in files:
-            if filename.endswith('.tif') or filename.endswith('.tiff'):
+            if filename.endswith('.tif') or filename.endswith('.tiff') or filename.endswith('.TIF'):
                 input_path = os.path.join(root, filename)
-
                 try:
                     img = Image.open(input_path)
                     output_path = os.path.splitext(input_path)[0] + '.jpg'
