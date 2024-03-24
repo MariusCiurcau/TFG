a0a7039f3f9e49f69f71a28fa6ad2a83714e4b5e
MariusCiurcau <mariusciurcau@gmail.com>
2024-03-24 01:21:16 +0100

    Fixed API key

--- Scripts/llm_gpt.py
+++ Scripts/llm_gpt.py
@@ -1,8 +1,8 @@
 from openai import OpenAI
-import openai
 
 # Set your OpenAI API key
-API_KEY = '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'
+with open("../key.txt", "r") as f:
+    API_KEY = f.read()
 
 def generate_explanations_gpt(original_explanation, versions):
     client = OpenAI(api_key=API_KEY)
@@ -17,9 +17,3 @@ def generate_explanations_gpt(original_explanation, versions):
         explanations[version] = generated_text
         print(f"Generated text with {version} expertise level: {generated_text}")
     return explanations
-
-# Example usage
-#original_text = 'A subcapital femoral neck fracture is a specific type of fracture that occurs in the femur bone, specifically in the region of the femoral neck, just below the femoral head. This fracture occurs within the hip joint and can be serious due to the disruption of blood supply to the femoral head, which can lead to complications such as avascular necrosis. Subcapital femoral neck fractures are common in older individuals, especially those with osteoporosis or decreased bone density, which makes bones more prone to fracturing with minimal trauma. These fractures typically occur after a fall or direct impact to the hip.'
-#expertise_levels = ['basic', 'intermediate', 'advanced']
-
-#generated_explanations = generate_explanations(original_text, expertise_levels)
