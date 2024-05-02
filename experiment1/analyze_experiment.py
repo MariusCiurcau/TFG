6b5865153770c0efa846fd3e3ff4c18ffacc4d10
MariusCiurcau <mariusciurcau@gmail.com>
2024-05-02 18:01:52 +0200

    Creados scripts para analizar experimentos

---
+++ experiment1/analyze_experiment.py
@@ -0,0 +1,40 @@
+import pandas
+import pandas as pd
+import requests
+from matplotlib import pyplot as plt
+
+file = "votesMulticlass.txt"
+url = "https://gaia.fdi.ucm.es/files/research/xai/xai-experiments/experiment1/" + file
+
+download = False
+
+def download_file(url, destination):
+    response = requests.get(url)
+    with open(destination, 'wb') as file:
+        file.write(response.content)
+
+if download:
+    download_file(url, file)
+
+
+
+# Read the text file into a DataFrame
+df = pd.read_csv(file, delimiter=';', names=["date", "ip", "knowledge", "role", "image", "method"])
+
+df['dataset'] = df['image'].str.split('_').str[0]
+
+df_group = df.groupby(['dataset', 'method']).size().reset_index(name='count')
+print(df_group)
+
+pivot_df = df_group.pivot(index='dataset', columns='method', values='count')
+relative_freq_df = pivot_df.div(pivot_df.sum(axis=1), axis=0)
+
+
+relative_freq_df.plot(kind='bar')
+plt.xlabel('Dataset')
+plt.ylabel('Percentage')
+plt.title('Percentage by Method and Dataset')
+plt.xticks(rotation=0)  # Rotate x labels
+plt.legend(title='Method')
+plt.tight_layout()
+plt.show()
---
+++ experiment3/analyze_experiment.py
@@ -0,0 +1,93 @@
+import os
+
+import pandas
+import pandas as pd
+import requests
+from matplotlib import pyplot as plt
+
+from Scripts.utils import read_label
+
+file = "votesMulticlass.txt"
+url = "https://gaia.fdi.ucm.es/files/research/xai/xai-experiments/experiment3/" + file
+IMG_DIR = '../Datasets/ULTIMAS/images'
+LABEL_DIR = '../Datasets/ULTIMAS/labels'
+download = False
+
+def download_file(url, destination):
+    response = requests.get(url)
+    with open(destination, 'wb') as file:
+        file.write(response.content)
+
+if download:
+    download_file(url, file)
+
+
+# Read the text file into a DataFrame
+df = pd.read_csv(file, delimiter=';', names=["date", "ip", "knowledge", "role", "image", "human_label"])
+
+
+true_labels = {}
+model_labels = {}
+with open('./images/images.csv', 'r') as f:
+    for line in f:
+        image = line.split(';')[0]
+        true_label = line.split(';')[1]
+        model_label = line.split(';')[2].strip()
+        true_labels[image] = true_label
+        model_labels[image] = model_label
+
+for i, row in df.iterrows():
+    image = row['image']
+    label = read_label(os.path.join(LABEL_DIR, os.path.splitext(image)[0] + '.txt'), 3)
+    print(image, label)
+
+
+df['true_label'] = df['image'].apply(lambda x: true_labels[x])
+
+dtypes = {
+    'human_label': int,
+    'true_label': int
+}
+df = df.astype(dtypes)
+
+
+df['p'] = (df['true_label'] == 1) | (df['true_label'] == 2)
+df['n'] = (df['true_label'] == 0)
+df['hp'] = (df['human_label'] == 1) | (df['human_label'] == 2)
+df['hn'] = df['human_label'] == 0
+
+df['correct'] = (((df['human_label'] == 1) | (df['human_label'] == 2)) & (df['p'])) | ((df['human_label'] == 0) & (df['n']))
+
+
+df['tp'] = df['hp'] & df['p']
+df['fp'] = df['hp'] & df['n']
+df['tn'] = df['hn'] & df['n']
+df['fn'] = df['hn'] & df['p']
+
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+    print(df[['human_label', 'true_label', 'correct', 'p', 'tp', 'fn']])
+human_precision = (df.groupby('role')['tp'].sum() + df.groupby('role')['tn'].sum()) / (df.groupby('role')['tp'].sum() + df.groupby('role')['fp'].sum() + df.groupby('role')['tn'].sum() + df.groupby('role')['fn'].sum())
+human_sensitivity = df.groupby('role')['tp'].sum() / (df.groupby('role')['tp'].sum() + df.groupby('role')['fn'].sum())
+pivot_table = pd.DataFrame({'accuracy': human_precision, 'sensitivity': human_sensitivity})
+
+
+model_tp = 0
+model_fp = 0
+model_tn = 0
+model_fn = 0
+for image, model_label in model_labels.items():
+    model_label = int(model_label)
+    true_label = int(true_labels[image])
+    if (model_label == 1 or model_label == 2) and (true_label == 1 or true_label == 2):
+        model_tp += 1
+    if model_label == 0 and true_label == 0:
+        model_tn += 1
+    if (model_label == 1 or model_label == 2) and true_label == 0:
+        model_fp += 1
+    if model_label == 0 and (true_label == 1 or true_label == 2):
+        model_fn += 1
+
+model_sensitivity = model_tp / (model_tp + model_fn)
+model_precision = (model_tp + model_tn) / (model_tp + model_fp + model_tn + model_fn)
+pivot_table.loc['Model'] = [model_precision, model_sensitivity]
+print(pivot_table)
